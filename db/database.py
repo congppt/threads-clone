3337cbadf6599f5b298a7427fbfe8638d11dcf4e
CongPPT <congppt2002@gmail.com>
2024-10-01 13:13:02 +0700

    sqlalchemy setup with async

---
+++ db/database.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = os.getenv("DATABASE")
+
+engine = create_async_engine(DATABASE_URL)
+
+AsyncSession = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False  # Prevent objects from being expired after commit
+)
+
+Base = declarative_base()
+
+async def get_db_async():
+    async with AsyncSession() as async_session:
+        async with async_session.begin() as session:
+            yield session
